048b7ae7987764ca2caffa9206d333db56d57095
juan-certacon <juan.marini@certacon.com.br>
2026-09-06 22:02:52 -0300

    estrutura inicial do projeto spring boot

---
+++ src/main/java/br/com/fiap/ms_educamais/MsEducamaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.ms_educamais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEducamaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEducamaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/ms_educamais/MsEducamaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.ms_educamais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEducamaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
